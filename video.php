8c5168c483147256282ecf7dba503b5dacd74f9b
ProdiGenius <yossilallouz@hotmail.com>
2017-08-01 14:31:43 -0400

    Update video.php

--- video.php
+++ video.php
@@ -29,6 +29,7 @@
 ?>
 
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<link rel="icon" href="favicon.png" type="image/png" sizes="32x32">
 <link rel="stylesheet" type="text/css" href="css/style.css"/>
 <link rel="stylesheet" type="text/css" href="css/style2.css"/>
 <link rel='stylesheet' type='text/css' href='css/style3.css'/>
